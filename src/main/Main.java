38499d7c9f80e73c5bd3ca53f7fcd9d215203620
lieo <lieoluntan@163.com>
2018-04-24 10:25:29 +0800

    基础版v1.0 可用+0424
    Signed-off-by: lieo <lieoluntan@163.com>

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+
+import java.io.IOException;
+
+import factory.create.CodeCreater;
+import freemarker.template.TemplateException;
+
+public class Main {
+	public static void main(String[] args) throws IOException, TemplateException, SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+		// xml文件位置
+//		String sourcePath = "C:/Users/Administrator/Desktop/dataExcel.xls";
+		String sourcePath = "G:/xppexcel/dataExcel.xls";
+		// 输出位置
+//		String writePath = "C:/Users/Administrator/Desktop/";
+		String writePath = "G:/xppcode/Desktop/";
+		
+		CodeCreater.StartCreate(sourcePath, writePath);
+	}
+}
